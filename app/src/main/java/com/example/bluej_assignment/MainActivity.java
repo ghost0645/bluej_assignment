9eda5a026fb26d81465678fcb4e8368c44c8e24e
ghost0645 <nickyxwar@gmail.com>
2021-02-21 02:23:49 +0800

    Initial commit

---
+++ app/src/main/java/com/example/bluej_assignment/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.bluej_assignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    //private ImageView wzz;
+
+
+    @Override
+   protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
